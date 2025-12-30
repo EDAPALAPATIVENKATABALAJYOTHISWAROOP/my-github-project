32d6d3412e35ef1e422168d24d6c95ce5ed4a6bf
EDAPALAPATIVENKATABALAJYOTHISWAROOP <edapalapatiswaroop@gmail.com>
2025-12-31 00:19:13 +0530

    Introduced bug in task list

--- todo_app/todo.py
+++ todo_app/todo.py
@@ -1,22 +1,2 @@
-tasks = []
-
-def add_task(task):
-    tasks.append(task)
-
-def show_tasks():
-    if not tasks:
-        print("No tasks available")
-    else:
-        for i, task in enumerate(tasks):
-            print(f"{i+1}. {task}")
-def delete_task(index):
-    if index < len(tasks):
-        tasks.pop(index)
-
-
-if __name__ == "__main__":
-    add_task("Learn Git")
-    delete_task(0)
-    show_tasks()
-
+tasks = None   # BUG
 
